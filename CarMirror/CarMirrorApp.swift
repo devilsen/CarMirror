21489b4b36bee4673b0a868f28aae1aa9d78ba19
CodeSam <375913436@qq.com>
2023-11-01 00:39:51 +0800

    Initial Commit

---
+++ CarMirror/CarMirrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarMirrorApp.swift
+//  CarMirror
+//
+//  Created by 董森 on 2023/11/1.
+//
+
+import SwiftUI
+
+@main
+struct CarMirrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarMirror/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CarMirror
+//
+//  Created by 董森 on 2023/11/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
